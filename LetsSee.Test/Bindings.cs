bf7321956d7e7f0cd1ab2824911636c8d99eb2ba
kaya grocott <kaya.grocott@fdmgroup.com>
2021-03-19 10:34:44 +1100

    initial commit

---
+++ LetsSee.Test/Bindings.cs
@@ -0,0 +1,36 @@
+﻿using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace LetsSee.Test
+{
+    public class Bindings
+    {
+        Class1 calculator = new Class1();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calculator.SecondNumber = number;
+        }
+
+        private int result;
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            result.Should().Be(result);
+        }
+    }
+}
+
